9c337da1a02c6423a1fddb8b7790aa8a70860993
Jacky Z. Chen <32407740+JackyC415@users.noreply.github.com>
2018-03-09 20:17:17 -0800

    self tests

---
+++ mini-mars/selftest.s
@@ -0,0 +1,37 @@
+.text
+main:
+_start:
+	addiu	$5,$0,4
+	addiu	$5,$5,100000	
+	addiu	$6,$6,-1
+	addiu	$6,$6,0x7fff
+	
+	addu	$6,$0,$0
+	addu	$6,$6,$5
+	addu	$6,$6,$5
+	subu	$6,$6,$5
+	addiu	$6,$6,5
+	addu	$6,$0,$0
+	addiu	$6,$6,16
+	sll	$5,$6,2
+	srl	$5,$6,4
+	addiu	$5,$0,3
+loop:	slt	$7,$6,$5
+	beq	$7,$0,branched
+	addiu	$5,$5,-1
+	j	loop
+	
+branched:
+
+	addu	$6,$0,$0
+	addu	$7,$0,$0
+	jr	$ra
+	
+	
+
+
+
+
+
+
+	
